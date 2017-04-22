d10572fe8635a88bd9ee7ff8acb78bcb6c77b6bf
Kim JungHyun <vizicube@gmail.com>
2017-04-22 22:00:38 +0900

    유니티 프로젝트 통으로 올림 10메가 이상힌 파일이 많지만 100메가는 넘지 않는 파일도 올라가는지를 테스트하기 위해서임

---
+++ Assets/VR World/Scripts/Test.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	public int numA ;
+	public int numB ;
+
+	int sumThis(int sumNA, int sumNB){
+		int result = sumNA + sumNB;
+		return result;
+	}
+
+	// Use this for initialization
+	void Start () {
+		int sumCnt = sumThis (numA,numB);
+		print (sumCnt);
+	}
+		
+}
