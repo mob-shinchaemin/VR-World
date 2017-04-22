fc5f262275a8a21f447a0eef418c65a01f11584d
kim junghyun <vizicube@gmail.com>
2017-04-23 02:32:03 +0900

    지형텍스쳐를 추가한 후에 거미와 이동관련 스크립트를 추가하고 VR 카메라를 거미의 자식으로 붙여놔서 거미의 움직임에 따라서 카메라가 거미를 따라서 이동을 한다.

---
+++ Assets/VR World/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	public float speed = 3f;
+	public float rotateSpeed = 30f;
+	Animation p_anim;
+
+	// Use this for initialization
+	void Start () {
+		p_anim = GetComponent<Animation> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W)) {
+			transform.Translate (0, 0, speed * Time.deltaTime);
+			p_anim.CrossFade ("walk");
+		}else if(Input.GetKey (KeyCode.S)){
+			transform.Translate (0, 0, -speed * Time.deltaTime);
+			p_anim.CrossFade ("walk");			
+		}else if(Input.GetKey(KeyCode.A)){
+			transform.Rotate (0,-rotateSpeed*Time.deltaTime,0);
+			p_anim.CrossFade ("walk");
+		}else if(Input.GetKey(KeyCode.D)){
+			transform.Rotate (0,rotateSpeed*Time.deltaTime,0);
+			p_anim.CrossFade ("walk");
+		}else {
+			p_anim.CrossFade ("idle");
+		}
+	}
+}
